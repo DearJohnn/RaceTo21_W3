8c195eb9103cd90423d0e21369ea21dbcf380eef
DearJohnn <sun.yuch@northeastern.edu>
2023-02-06 23:44:57 -0500

    1

---
+++ Deck.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // currently only needed if we use alternate shuffle method
+
+namespace RaceTo21
+{
+    public class Deck
+    {
+        List<Card> cards = new List<Card>();
+        // Create a Dictionary to store two string type date, one is card image file name another is card id 
+        Dictionary<string, string> imagesDictionary = new Dictionary<string, string>();
+        public Deck()
+        {
+            Console.WriteLine("*********** Building deck...");
+            string[] suits = { "Spades", "Hearts", "Clubs", "Diamonds" };
+
+            for (int cardVal = 1; cardVal <= 13; cardVal++)
+            {
+                foreach (string cardSuit in suits)
+                {
+                    string cardName;
+                    string cardLongName;
+                    string cardID;
+                    string imageID;
+
+
+                    switch (cardVal)
+                    {
+                        case 1:
+                            cardName = "A";
+                            cardLongName = "Ace";
+                            break;
+                        case 11:
+                            cardName = "J";
+                            cardLongName = "Jack";
+                            break;
+                        case 12:
+                            cardName = "Q";
+                            cardLongName = "Queen";
+                            break;
+                        case 13:
+                            cardName = "K";
+                            cardLongName = "King";
+                            break;
+                        default:
+                            cardName = cardVal.ToString();
+                            cardLongName = cardName;
+                            break;
+                    }
+                    // Generate card image file name
+                    imageID = "card_" + cardSuit + "_" + cardName + ".png";
+                    cardID = cardName + cardSuit.First<char>();
+                    cards.Add(new Card(cardID, cardLongName + " of " + cardSuit));
+                    imagesDictionary.Add(cardID, imageID);
+                    Console.WriteLine( $"{imagesDictionary[cardID]}");
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Console.WriteLine("Shuffling Cards...");
+
+            Random rng = new Random();
+
+            // one-line method that uses Linq:
+            // cards = cards.OrderBy(a => rng.Next()).ToList();
+
+            // multi-line method that uses Array notation on a list!
+            // (this should be easier to understand)
+            for (int i=0; i<cards.Count; i++)
+            {
+                Card tmp = cards[i];
+                int swapindex = rng.Next(cards.Count);
+                cards[i] = cards[swapindex];
+                cards[swapindex] = tmp;
+
+                //cards[i] = cards[swapindex]; // makes a duplicate
+                //cards[swapindex] = cards[i]; //doesn't change anything
+            }
+        }
+
+        /* Maybe we can make a variation on this that's more useful,
+         * but at the moment it's just really to confirm that our 
+         * shuffling method(s) worked! And normally we want our card 
+         * table to do all of the displaying, don't we?!
+         */
+
+        public void ShowAllCards()
+        {
+            for (int i=0; i<cards.Count; i++)
+            {
+                Console.Write(i+":"+cards[i].displayName); // a list property can look like an Array!
+                if (i < cards.Count -1)
+                {
+                    Console.Write(" ");
+                } else
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        public Card DealTopCard()
+        {
+            Card card = cards[cards.Count - 1];
+            cards.RemoveAt(cards.Count - 1);
+            // Console.WriteLine("I'm giving you " + card);
+            return card;
+        }
+    }
+}
+
