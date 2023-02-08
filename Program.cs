b9752550a96eadd8ec4464f3a6d41bb568cdaea0
DearJohnn <sun.yuch@northeastern.edu>
2023-02-08 15:03:50 -0500

    Draft

--- Program.cs
+++ Program.cs
@@ -13,6 +13,17 @@ namespace RaceTo21
                 game.DoNextTask();
             }
         }
+        /********* Draft *********
+         
+         *Level 1: 
+                    Design a chip account for each player. Players can bet each round, and the winner wins all the chips. 
+                    The player's bet is kept for the next turn if no one wins this turn.
+         *Level 2：
+                   The person with the most bets in the wager account after three rounds wins, and if a player loses all 
+                   bets within a period of 3 rounds, the game ends immediately. 
+         
+         
+         */
     }
 }
 
